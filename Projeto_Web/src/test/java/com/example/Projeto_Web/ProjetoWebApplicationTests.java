541ca6012cee95510d642132de7db009042cfce5
JohnSCastro <johnbsilva18@gmail.com>
2024-06-01 17:35:25 -0400

    Inicialização do Projeto, criação dos arquivos via Spring Tools

---
+++ Projeto_Web/src/test/java/com/example/Projeto_Web/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
